b639eb1d7a0fd6b163b680668b009083d4bbe891
Cory Dransfeldt <hi@coryd.dev>
2024-12-10 17:23:18 -0800

    feat: new homepage

---
+++ src/data/recentMedia.js
@@ -0,0 +1,20 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export default async function fetchRedirects() {
+  const { data, error } = await supabase
+    .from("optimized_recent_media")
+    .select("*");
+
+  if (error) {
+    console.error("Error fetching recent media data:", error);
+    return [];
+  }
+
+  const [{ recent_activity } = {}] = data;
+
+  return recent_activity || [];
+}
